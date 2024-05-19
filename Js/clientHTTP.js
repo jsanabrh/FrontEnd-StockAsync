1cdd1379dbfd51cd55e5066f0719a8d1acd42dd6
Circirus97 <msuarezalejandra@gmail.com>
2024-05-19 09:24:24 -0500

    Se conecta billing front con invoice Back

--- Js/clientHTTP.js
+++ Js/clientHTTP.js
@@ -25,6 +25,10 @@ export async function get(url) {
 
     try {
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         const data = await response.json();
 
         return data;
