9b264b19775783ff496beaf014a3bc677c0c17f6
miguelangito <miguelangelespinalospina@gmail.com>
2024-05-19 21:15:02 -0500

    actu

--- Js/adminInventary.js
+++ Js/adminInventary.js
@@ -1,70 +1,60 @@
-import { deleteHttp, post, update } from "../../API/clientHTTP.js"
-import { get } from "../../API/clientHTTP.js"
-import { URL_INVENTARY,URL_PRODUCTS } from "../../API/URLS.js";
-import { idCategoryUpdate, printCategories } from "./functionsDOM.js";
+import { get, deleteHttp, post, update } from "./clientHTTP.js";
+import { URL_INVENTARY, URL_PRODUCTS } from "./urls.js";
+import { printProducts } from "./functions.js";
 
-//Selectores
+// Selectores
 
-const formCategories = document.getElementById("formCategories");
 export const nameCategory = document.getElementById("nameCategory");
 export const descriptionCategory = document.getElementById("descriptionCategory");
 
+// Inicialización
+document.addEventListener("DOMContentLoaded", () => {
 
-//Eventos
-
-formCategories.addEventListener("submit", (event)=>{
-    event.preventDefault();
-
-    if(idCategoryUpdate.value){
-        //Actualizar
-        updateProduct(idCategoryUpdate.value);
-    }else {
-        createProduct();
-    }
-
+    getProducts()
 });
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    getProduct();
+// // Eventos
+// formCategories.addEventListener("submit", (event) => {
+//     event.preventDefault();
 
-})
+//     if (idCategoryUpdate.value) {
+//         // Actualizar
+//         updateProduct(idCategoryUpdate.value);
+//     } else {
+//         createProduct();
+//     }
+// });
 
+// Funciones
 async function createProduct() {
-
     const newProduct = {
-        name:"",
-        price:"",
-        size:"",
-        color:"",
-        stock:"",
-        inventaryId:""
+        name: "",
+        price: "",
+        size: "",
+        color: "",
+        stock: "",
+        inventaryId: ""
     };
 
-    post(URL_PRODUCTS, newProduct);
+    await post(URL_PRODUCTS, newProduct);
 }
 
+async function getProducts(){
+    const data = get(URL_PRODUCTS)
 
-async function getProduct() {
-   const data = await get(URL_INVENTARY);
-
-   console.log(data);
-
-   printCategories(data);
+    printProducts(data)
 }
 
 export async function deleteProduct(id) {
     console.log("Eliminado Id, ", id);
-
     await deleteHttp(`${URL_PRODUCTS}/${id}`);
 }
 
 export async function updateProduct(id) {
-
     const productUpdate = {
         name: nameCategory.value,
-        description : descriptionCategory.value
+        description: descriptionCategory.value
     };
 
-    await update(`${URL_CATEGORIES}/${id}`, categoryUpdate);
+    await update(`${URL_CATEGORIES}/${id}`, productUpdate);
 }
-
--- Js/functions.js
+++ Js/functions.js
@@ -1,50 +1,21 @@
-import { deleteNew } from "./index.js";
+const tableProducts = document.getElementById("tableProducts");
 
-const btnOpenModalNews = document.getElementById("btnOpenModalNews");
+export function printProducts(products) {
 
-export function printNews(news) {
+  tableProducts.innerHTML = "";
 
-    news.forEach((tempNew) => {
-        const tr = document.createElement("tr");
+    Array(products).forEach((product) => {
 
-        const tdId = document.createElement("td");
-        const tdContent = document.createElement("td");
-        const tdQuantity = document.createElement("td");
-        const tdPrice = document.createElement("td");
+      const row = document.createElement("tr");
 
-        const btnEdit = document.createElement("button");
-        const btnDelete = document.createElement("button");
+        row.innerHTML +=
+        `<tr>
+        <th scope="row">${product.id}</th>
+        <td colspan="2">${product.name}</td>
+        <td>${product.stock}</td>
+        <td>${product.price}</td>
+      </tr>`;
 
-        btnDelete.classList.add("btn", "btn-danger");
-        btnEdit.classList.add("btn", "btn-primary");
-
-        btnEdit.textContent = "Edit";
-        btnDelete.textContent = "Delete";
-
-        btnEdit.addEventListener("click", ()=> {
-            loadInfoNew(tempNew)
-        });
-
-        btnDelete.addEventListener("click", ()=> {
-            deleteNew(tempNew.id)
-
-        });
-
-        tdId.textContent = tempNew.name;
-        tdContent.textContent = tempNew.content;
-        tdQuantity.textContent = tempNew.publicationDate;
-        tdPrice.textContent = tempNew.user.name;
-
-        tdActions.appendChild(btnEdit);
-        tdActions.appendChild(btnDelete);
-
-        tr.appendChild(tdId);
-        tr.appendChild(tdContent);
-        tr.appendChild(tdQuantity);
-        tr.appendChild(tdPrice);
-
-
-        newsTbody.appendChild(tr);
-
+      tableProducts.appendChild(row);
     });
 }
